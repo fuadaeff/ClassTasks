751b389fe078632df3a08d1f6a4655dda4bcc5d8
fuadaeff <fabdulla057@gmail.com>
2025-02-27 21:31:57 +0400

    Add files via upload

---
+++ carClass/carClass/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace carClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mercedes", 2021);
+            car1.print();
+        }
+        public class Car
+        {
+            public string brand;
+            public int year;
+            public Car(string Brand, int Year) {
+                brand = Brand;
+                year = Year;
+            }
+            public void print() {
+                Console.WriteLine($"Marka: {brand}, Il: {year}");
+            }
+        }
+    }
+}
