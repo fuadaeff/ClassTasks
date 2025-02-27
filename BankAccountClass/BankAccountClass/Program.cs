6eca5f810d92b808cf1885582c558bce8b0e0fc1
fuadaeff <fabdulla057@gmail.com>
2025-02-27 21:47:17 +0400

    Add files via upload

--- BankAccountClass/BankAccountClass/Program.cs
+++ BankAccountClass/BankAccountClass/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //BankAccount account1 = new BankAccount(-50);
+            BankAccount account1 = new BankAccount(50);
+            account1.ShowBalance();
+            account1.Deposit(50);
+            account1.Withdraw(30);
+            account1.Withdraw(150);
+        }
+        class BankAccount
+        {
+            private decimal balance;
+
+            public BankAccount(decimal Balance)
+            {
+                if (Balance >= 0)
+                {
+                    balance = Balance;
+                }
+                else
+                {
+                    Console.WriteLine("Balans menfi ola bilmez,0 teyin olundu.");
+                    balance = 0;
+                }
+            }
+
+            public void Deposit(decimal amount)
+            {
+                if (amount > 1)
+                {
+                    balance += amount;
+                    Console.WriteLine($"Hesaba {amount} AZN elave olundu. Yeni balans: {balance} AZN-dir.");
+                }
+                else
+                {
+                    Console.WriteLine("Minimum 1 AZN!");
+                }
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                if (amount > 0)
+                {
+                    if (amount <= balance)
+                    {
+                        balance = balance - amount;
+                        Console.WriteLine($"Hesabdan {amount} AZN cixarildi. Yeni balans: {balance} AZN-dir.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Balansda kifayet qeder mebleg yoxdur!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Cixaris meblegi musbet olmalidir!");
+                }
+            }
+
+            public void ShowBalance()
+            {
+                Console.WriteLine($"Cari balans: {balance} AZN-dir");
+            }
         }
     }
 }
