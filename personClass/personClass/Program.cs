fecfcdd20eeb7331d8ca0f78826a04ae42214554
fuadaeff <fabdulla057@gmail.com>
2025-02-27 21:31:24 +0400

    Add files via upload

---
+++ personClass/personClass/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace personClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Fuad", 18);
+            person1.Print();
+        }
+        public class Person
+        {
+            public string Name;
+            public int Age;
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"Ad: {Name}, Yaş: {Age}");
+            }
+        }
+    }
+}
