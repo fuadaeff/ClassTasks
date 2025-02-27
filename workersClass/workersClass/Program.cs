38b08d0694753b2deed3524ee9519125563226a6
fuadaeff <fabdulla057@gmail.com>
2025-02-27 21:34:02 +0400

    Add files via upload

---
+++ workersClass/workersClass/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace workersClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker1 = new Worker("Fuad",1800,"IT");
+            worker1.GetWorkerInfo("Fuad",1800,"IT");
+            worker1.IncreaseSalary(1800);
+        }
+        public class Worker
+        {
+            public string Name; 
+            public int Salary;
+            public string Position;
+            public Worker(string name, int salary, string position) {
+                Name = name;
+                Salary = salary;
+                Position = position;
+            }
+            public void IncreaseSalary(int Salary) {
+                Salary = Salary + 200;
+                Console.WriteLine($"Iscinin yeni maasi: {Salary}");
+            }
+            public void GetWorkerInfo(string Name, int Salary, string Position) {
+                Console.WriteLine($"Iscinin adi: {Name},pozisiyasi: {Position}, maasi: {Salary}");
+            }
+        }
+    }
+}
